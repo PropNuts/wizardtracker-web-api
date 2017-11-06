7a2b5b92d01aa6a324bbdae1a50a905d92612565
Daniel Callander <knifacat@gmail.com>
2017-11-06 03:52:39 +0000

    Send filtered RSSI over WS

--- app/data_streamer.py
+++ app/data_streamer.py
@@ -23,6 +23,9 @@ class DataStreamer:
         self._should_stop = False
         self._last_message_time = time.clock()
 
+        self._rssi_raw = None
+        self._rssi_filtered = None
+
     def start(self):
         self._redis = redis.StrictRedis(
             decode_responses=True)
@@ -30,6 +33,7 @@ class DataStreamer:
 
         self._redis_pubsub = self._redis.pubsub(ignore_subscribe_messages=True)
         self._redis_pubsub.subscribe('rssiRaw')
+        self._redis_pubsub.subscribe('rssiFiltered')
 
         while not self._should_stop:
             self._loop()
@@ -39,12 +43,24 @@ class DataStreamer:
 
     def _loop(self):
         message = self._redis_pubsub.get_message()
-        if message and message['channel'] == 'rssiRaw':
+        if message:
             data = json.loads(message['data'])
 
-            if self._due_next_message():
-                self._socketio.emit('rssi', {'rssi': data['rssi']})
-                self._last_message_time = time.clock()
+            if message['channel'] == 'rssiRaw':
+                self._rssi_raw = data['rssi']
+            elif message['channel'] == 'rssiFiltered':
+                self._rssi_filtered = data['rssi']
+
+            self._tick_messages()
+
+    def _tick_messages(self):
+        if not self._rssi_raw or not self._rssi_filtered:
+            return
+
+        if self._due_next_message():
+            self._socketio.emit('rssiRaw', {'rssi': self._rssi_raw})
+            self._socketio.emit('rssiFiltered', {'rssi': self._rssi_filtered})
+            self._last_message_time = time.clock()
 
     def _due_next_message(self):
         message_delay = 1 / DataStreamer.MESSAGES_PER_SECOND
